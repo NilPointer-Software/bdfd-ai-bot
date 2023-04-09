8a4ae728b483ac49f7fc5df033c1feaa6a58fde7
Jakub Tomana <jakub.tomana@protonmail.com>
2023-04-09 16:05:05 +0200

    feat: Add multi-level logging

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
+use flexi_logger::Logger;
 use shaku::{HasComponent, module};
-use crate::core::app_config::{AppConfigServiceImpl};
-use crate::core::network_module::{NetworkModuleImpl};
+
+use crate::core::app_config::AppConfigServiceImpl;
+use crate::core::network_module::NetworkModuleImpl;
 use crate::data::ai::ai_repository::AIRepositoryImpl;
 use crate::domain::ai::use_cases::generate_first_message_help_use_case::GenerateFirstMessageHelpUseCaseImpl;
-use crate::domain::use_cases::get_discord_client_use_case::{GetDiscordClientUseCaseImpl};
 use crate::domain::use_cases::get_app_config_use_case::GetAppConfigUseCaseImpl;
+use crate::domain::use_cases::get_discord_client_use_case::GetDiscordClientUseCaseImpl;
 use crate::infrastructure::discord_bot::{DiscordBot, DiscordBotImpl};
 
 mod core;
@@ -30,6 +32,8 @@ module! {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    Logger::try_with_env_or_str("info, tracing::span=warn").unwrap().start().unwrap();
+
     let module = MainModule::builder().build();
     let discord_bot: &dyn DiscordBot = module.resolve_ref();
     discord_bot.start_bot().await?;
